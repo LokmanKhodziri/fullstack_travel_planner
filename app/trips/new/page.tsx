337283dacf80be63f2d7433e90d25e71275fa51e
lokmankhodziri <lokmankhodziri@gmail.com>
2025-07-19 08:41:22 +0800

    Add lib/utils.ts with cn utility function to fix import error

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export default function NewTrips() {
+  return (
+    <div className="max-w-lg mx-auto mt-10">
+      <Card>
+        <CardHeader>New Trip</CardHeader>
+        <CardContent>
+          <form className="space-y-6" action="">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                required
+                placeholder="Trip Name..."
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Description
+              </label>
+              <textarea
+                name="description"
+                required
+                placeholder="Trip Detail"
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+              />
+            </div>
+            <div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Start Date
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  required
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  End Date
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  required
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
